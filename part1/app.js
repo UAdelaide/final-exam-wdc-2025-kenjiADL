5801ee512737f54fa4534fed70b90f7ee3ce9ce4
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 04:27:06 +0000

    [automatic commit] 2025-06-20 04:27:06 UTC 14.203.54.57

--- part1/app.js
+++ part1/app.js
@@ -47,6 +47,6 @@ app.get('/api/dogs', (req, res) => {
 });
 
 app.get('/qpi/walkrequests/open', (req, res) => {
-    const sql = 'SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_na'
+    const sql = 'SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = '
 })
 module.exports = app;
