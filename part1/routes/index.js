f984416dca101b070046555e01ccfbc05fa4d395
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 03:46:14 +0000

    [automatic commit] 2025-06-20 03:46:14 UTC 14.203.54.57

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+  host: 'localhost'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
