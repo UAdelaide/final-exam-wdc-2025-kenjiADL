b2cc64df97d506f69e04fc21d1c54ae5d8877bef
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 02:21:49 +0000

    [automatic commit] 2025-06-20 02:21:49 UTC 14.203.54.57

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -67,3 +67,4 @@ VALUES
 ((SELECT user_id FROM Users WHERE  username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'ken987'), 'Rocky', 'large'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), )
