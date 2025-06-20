7f27709fbf2e6841b8909128a826d3127f2b7140
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 09:22:23 +0000

    [automatic commit] 2025-06-20 09:22:22 UTC 14.203.54.57

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,6 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // ✅ Save user to session
     req.session.user = rows[0];
 
     res.json({ message: 'Login successful', user: rows[0] });
