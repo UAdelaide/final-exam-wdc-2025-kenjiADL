a197226912705f248ec68c24faa2a3f5777bd6cb
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 02:32:58 +0000

    [automatic commit] 2025-06-20 02:32:58 UTC 14.203.54.57

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -57,8 +57,8 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-('bobwalker' 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed 780', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed780', 'owner'),
 ('ken987', 'ken@example.com', 'hashed777', 'owner'),
 ('john489', 'john@example.com', 'hashed911', 'walker');
 
