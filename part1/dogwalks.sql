a4a5dafd3cf11af77f25249a1a801320449ab772
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 02:20:13 +0000

    [automatic commit] 2025-06-20 02:20:13 UTC 14.203.54.57

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -65,4 +65,4 @@ VALUES
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
 ((SELECT user_id FROM Users WHERE  username = 'alice123'), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username ='carol123'))
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
