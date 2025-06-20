e9b2e9844b75878338e7560bf8b48fa1203c07c0
Kenji Baritua <kenji.baritua@student.adelaide.edu.au>
2025-06-20 10:20:28 +0000

    [automatic commit] 2025-06-20 10:20:21 UTC 120.20.143.145

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,7 +58,7 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed780', 'owner'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('ken987', 'ken@example.com', 'hashed777', 'owner'),
 ('john489', 'john@example.com', 'hashed911', 'walker');
 
